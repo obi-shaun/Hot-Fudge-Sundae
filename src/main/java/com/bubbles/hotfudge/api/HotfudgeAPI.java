85aa1aaa3fa20baee255636fa2be6071b83a27f4
Shaun <shaun.nasiri2@gmail.com>
2016-05-31 22:54:32 -0400

    Creating additional endpoints and updating TODOs for HotfudgeAPI.java.

--- src/main/java/com/bubbles/hotfudge/api/HotfudgeAPI.java
+++ src/main/java/com/bubbles/hotfudge/api/HotfudgeAPI.java
@@ -7,11 +7,28 @@ import static spark.Spark.post;
 public class HotfudgeAPI {
 	
 	public static void main(String[] args) {
-		get("/home", (req, res) -> {
-			return "Hot Fudge Sundae under construction";
+		get("/sundaes", (req, res) -> {
+			//TODO: retrieve all HotFudgeSundaes
+			return null;
 		});
 		
-		post("/hotfudgesundaes", (req, res) -> {
+		get("/sundaes/:id", (req, res) -> {
+			//TODO: retrieve HotFudgeSundae with given id
+			return null;
+		});
+		
+		post("/sundaes", (req, res) -> {
+			//TODO: create a new HotFudgeSundae
+			return null;
+		});
+		
+		get("/reviews/:sundaeId", (req, res) -> {
+			//TODO: retrieve all reviews for a HotFudgeSundae with given id
+			return null;
+		});
+		
+		post("/reviews", (req, res) -> {
+			//TODO: create a review for a given HotFudgeSundae
 			return null;
 		});
 	}
