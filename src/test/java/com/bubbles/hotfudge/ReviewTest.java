f703120b5388ceacd370beee2d798a1f5e676378
obi-shaun <shaun.nasiri2@gmail.com>
2016-06-05 02:42:05 -0400

    Refactored ReviewTest.java to match changes made to Review.java constructor and to take advantage of dependency injection in GenericReviewDAO.java

--- src/test/java/com/bubbles/hotfudge/ReviewTest.java
+++ src/test/java/com/bubbles/hotfudge/ReviewTest.java
@@ -3,27 +3,33 @@ package com.bubbles.hotfudge;
 import static org.junit.Assert.*;
 
 import org.apache.commons.lang3.RandomStringUtils;
-
+import org.junit.Before;
 import org.junit.Test;
 
 public class ReviewTest {	
 	
-	//TODO: Come up with more descriptive name
+	private HotFudgeSundae sundae;
+	
+	@Before
+	public void initialize() {
+		sundae = new HotFudgeSundae("Delia's", true, 5.99);
+		sundae.setId(0);
+	}
+		
 	@Test
-	public void positiveTest() {
+	public void createAReviewWithValidArguments() {
 		int validCommentLength = 50;
 		int validRating = 5;
-		Review validReview = new Review(RandomStringUtils.random(validCommentLength), validRating);
+		Review validReview = new Review(RandomStringUtils.random(validCommentLength), validRating, sundae.getId());
 		assertEquals(validRating, validReview.getRating());
 		assertEquals(validCommentLength, validReview.getComment().length());
 	}
 	
-	//TODO: Come up with more descriptive name
 	@Test
-	public void negativeTest() {
+	public void createAReviewWithInvalidArguments() {
 		int invalidCommentLength = 200;
 		int invalidRating = -5;
-		Review invalidReview = new Review(RandomStringUtils.random(invalidCommentLength), invalidRating);
+		Review invalidReview = new Review(RandomStringUtils.random(invalidCommentLength), invalidRating, sundae.getId());
 		assertEquals(Review.getMinimumRating(), invalidReview.getRating());
 		assertEquals(Review.getCharacterLimit(), invalidReview.getComment().length());
 	}
