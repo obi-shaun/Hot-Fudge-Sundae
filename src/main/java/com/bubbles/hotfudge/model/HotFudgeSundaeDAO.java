6276698b168f518ac4b846077c37268bf015f445
Shaun <shaun.nasiri2@gmail.com>
2016-06-06 23:21:28 -0400

    todo comment for handling exceptions when serializing and deserializing objects to and from json

--- src/main/java/com/bubbles/hotfudge/model/HotFudgeSundaeDAO.java
+++ src/main/java/com/bubbles/hotfudge/model/HotFudgeSundaeDAO.java
@@ -3,6 +3,7 @@ package com.bubbles.hotfudge.model;
 import java.util.List;
 import com.bubbles.hotfudge.HotFudgeSundae;
 
+//TODO: Exception handling
 public interface HotFudgeSundaeDAO {
 	public void add(HotFudgeSundae sundae);
 	public HotFudgeSundae find(int id);
--- src/main/java/com/bubbles/hotfudge/model/ReviewDAO.java
+++ src/main/java/com/bubbles/hotfudge/model/ReviewDAO.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import com.bubbles.hotfudge.Review;
 
+//TODO: Exception handling
 public interface ReviewDAO {
 	public void add(Review review);
 	public List<Review> findAll(int sundaeId);
