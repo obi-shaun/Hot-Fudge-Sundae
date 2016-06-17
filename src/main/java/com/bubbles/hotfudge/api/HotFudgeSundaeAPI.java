29f87ee22cc9ab0faa30e73cdc514af5e4789803
Shaun <shaun.nasiri2@gmail.com>
2016-06-17 18:43:25 -0400

    Created class of constants to store content types

--- src/main/java/com/bubbles/hotfudge/api/HotFudgeSundaeAPI.java
+++ src/main/java/com/bubbles/hotfudge/api/HotFudgeSundaeAPI.java
@@ -6,13 +6,13 @@ import static spark.Spark.post;
 
 import java.util.ArrayList;
 
+import com.bubbles.hotfudge.constants.ContentType;
 import com.bubbles.hotfudge.dao.impl.GenericHotFudgeSundaeImpl;
 import com.bubbles.hotfudge.dao.impl.GenericReviewImpl;
 import com.bubbles.hotfudge.model.HotFudgeSundae;
 import com.bubbles.hotfudge.model.Review;
 import com.bubbles.hotfudge.service.HotFudgeSundaeService;
 import com.bubbles.hotfudge.service.ReviewService;
-
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 
@@ -21,7 +21,6 @@ public class HotFudgeSundaeAPI {
 	//TODO: Create API tests
 	//TODO: Implement exception handling
 	//TODO: Return errors wrapped in json
-	//TODO: Create enums for content type and status codes
 	//TODO: Swap generic impls with database impls
 	public static void main(String[] args) {
 		
@@ -37,25 +36,25 @@ public class HotFudgeSundaeAPI {
 		
 		ObjectMapper objMapper = new ObjectMapper();
 								
-		get("/sundaes", "application/json", (req, res) -> {
+		get("/sundaes", ContentType.APPLICATION_JSON, (req, res) -> {
 			return sundaeService.getSundaes();
 		}, objMapper::writeValueAsString);
 		
-		post("/sundaes", "application/json", (req, res) -> {
+		post("/sundaes", ContentType.APPLICATION_JSON, (req, res) -> {
 			HotFudgeSundae sundae = objMapper.readValue(req.body(), HotFudgeSundae.class);
 			sundaeService.addNewSundae(sundae);
 			return sundae;
 		}, objMapper::writeValueAsString);
 		
-		get("/sundaes/:id", "application/json", (req, res) -> {
+		get("/sundaes/:id", ContentType.APPLICATION_JSON, (req, res) -> {
 			return sundaeService.getSundae(Integer.parseInt(req.params("id")));
 		}, objMapper::writeValueAsString);
 		
-		get("/sundaes/:id/reviews", "application/json", (req, res) -> {
+		get("/sundaes/:id/reviews", ContentType.APPLICATION_JSON, (req, res) -> {
 			return reviewService.getReviewsForSundae(Integer.parseInt(req.params("id")));
 		}, objMapper::writeValueAsString);
 		
-		post("/sundaes/:id/reviews", "application/json", (req, res) -> {
+		post("/sundaes/:id/reviews", ContentType.APPLICATION_JSON, (req, res) -> {
 			int sundaeId = Integer.parseInt(req.params("id"));
 			Review review = objMapper.readValue(req.body(), Review.class);
 			reviewService.addReview(review, sundaeId);
@@ -63,7 +62,7 @@ public class HotFudgeSundaeAPI {
 		}, objMapper::writeValueAsString);
 		
 		after((req, res) -> {
-			res.type("application/json");
+			res.type(ContentType.APPLICATION_JSON);
 		});
 
 	}
---
+++ src/main/java/com/bubbles/hotfudge/constants/ContentType.java
@@ -0,0 +1,14 @@
+package com.bubbles.hotfudge.constants;
+
+public class ContentType {
+	
+	/*
+	 * This class contains the
+	 * Content Types for use in HotFudgeSundae
+	 * stored as constants
+	 */
+	public static final String 
+		APPLICATION_JSON = "application/json", 
+		TEXT_HTML = "text/html";
+
+}
