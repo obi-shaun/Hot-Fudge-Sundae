dfcaadd1d30f2503e6087e84d82cfbe2a0995234
obi-shaun <shaun.nasiri2@gmail.com>
2016-07-09 16:07:41 -0400

    Constants to hold HTTP Status codes

--- src/main/java/com/bubbles/hotfudge/constants/HttpStatusCode.java
+++ src/main/java/com/bubbles/hotfudge/constants/HttpStatusCode.java
@@ -2,9 +2,11 @@ package com.bubbles.hotfudge.constants;
 
 public class HttpStatusCode {
 	
-	//TODO: Create enum that contains these codes and their associated msgs
+	//TODO: Create enum that contains these codes and their associated msgs.
+	//TODO: Create enums package for above ^
 	public static final int
-		RESOURCE_NOT_FOUND = 404,
-		INTERNAL_ERROR = 500;
-
+		OK = 200;
+		NOT_FOUND = 404,
+		INTERNAL_SERVER_ERROR = 500;
+		
 }
